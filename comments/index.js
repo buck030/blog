eb080a6f49b2ee0eed4345be85ef98de60abaf29
Dennis de Buck <dennis.de.buck@theorangeforce.com>
2025-07-03 20:54:52 +0200

    Started on Microservices course assignment 1

---
+++ comments/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');  // Importing necessary modules
+
+const commentsByPostId = {};  // Object to store comments by post ID
+
+const app = express();
+app.use(bodyParser.json());  // Middleware to parse JSON bodies
+
+  
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');  // Generate a random ID for the comment
+    const { content } = req.body;  // Extract content from the request body
+
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({ id: commentId, content});
+
+    commentsByPostId[req.params.id] = comments;
+    res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+    console.log('Listening on port 4001');
+});  // Start the server on port 4001  
--- posts/index.js
+++ posts/index.js
@@ -14,7 +14,8 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-
+// Post a new post
+// Generates a random ID for the post and adds it to the posts object
 app.post('/posts', (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
@@ -23,6 +24,8 @@ app.post('/posts', (req, res) => {
     res.status(201).send(posts[id]);
 });
 
+// listen on port 4000
+// This is where the server starts listening for incoming requests
 app.listen(4000, () => {
     console.log('Listening on port 4000');
 });
