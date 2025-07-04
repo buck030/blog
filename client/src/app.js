4a8ccf2368756a9d2be2b882dbf82c7891c5b6ef
Dennis de Buck <dennis.de.buck@theorangeforce.com>
2025-07-04 10:16:33 +0200

    Added postCreate and daisy packaging

---
+++ client/src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import postCreate from "./postCreate";
+
+const App = () => {
+    return <div>
+        <h1>Create Post</h1>
+        <postCreate />
+    </div>
+}
+
+export default App;
---
+++ client/src/postCreate.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const postCreate = () => {
+    return <div>
+        <form>
+            <div className="">
+                <label>Title</label>
+                <input className="" />     
+            </div>
+            <button className=""></button>
+        </form>
+    </div>
+}
+
+export default postCreate;
